1d3d676b290998e70f244bfd05119ae92c9c0a8b
EduardoLPaez <edpaez54g5@gmail.com>
2020-04-13 11:12:06 -0400

    tested pandas_datareader as source, created jupyternb to test data,adn build base functions.

--- start.py
+++ start.py
@@ -1,3 +1,13 @@
 import pandas as pd
 import numpy as np
 
+import pandas_datareader as pdr
+
+pdr.get_data_fred('GS10')
+
+
+start_ = '2017-04-22'
+end_ = '2018-04-22'
+
+df = pdr.data.DataReader(name='AMZN',data_source= 'yahoo', start=start_, end = end_)
+print(df)
